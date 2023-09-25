267093a333aec848496267ce20c34890743a32a6
TheoRusu <theorusu11@gmail.com>
2023-09-25 19:31:25 -0400

    App 404 unit test added

---
+++ tests/unit/app.test.js
@@ -0,0 +1,12 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('HTTP Unit Test', () => {
+  test('404 handler is being hit', async () => {
+    const res = await request(app).get('/fake');
+    expect(res.status).toBe(404);
+  });
+});
