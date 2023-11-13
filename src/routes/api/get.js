1b3d66deadcfe9d0d1f56b765aac35fc02459352
TheoRusu <theorusu11@gmail.com>
2023-11-13 17:38:20 -0500

    Implemented GET expand query

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -6,7 +6,16 @@ const { createSuccessResponse } = require('../../response');
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
-  const fragments = await Fragment.byUser(req.user, false);
+  const expandQueryParam = req.query.expand;
+  // logger.debug(expandQueryParam, 'EXPAND QUERY PARAM');
+
+  let fragments = {};
+  if (expandQueryParam == 1) {
+    fragments = await Fragment.byUser(req.user, true);
+  } else {
+    fragments = await Fragment.byUser(req.user, false);
+  }
+
   logger.debug(fragments, 'GET - Fragments array returned');
   res.status(200).json(createSuccessResponse({ fragments: fragments }));
 };
--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 
 const app = require('../../src/app');
+const hash = require('../../src/hash');
 
 describe('GET /v1/fragments', () => {
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
@@ -28,5 +29,22 @@ describe('GET /v1/fragments', () => {
     const res1 = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
 
     expect(res1.body.fragments.length).toBe(1);
+  });
+
+  test('posting a fragment and getting it expanded', async () => {
+    const res = await request(app)
+      .post('/v1/fragments?expand=1')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('Test Fragment');
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.status).toBe('ok');
+
+    const res1 = await request(app)
+      .get('/v1/fragments?expand=1')
+      .auth('user1@email.com', 'password1');
+
+    expect(res1.body.fragments[0].ownerId).toEqual(hash('user1@email.com'));
   });
 });
